cae67a2689d0bc038255b043dd6489c50c836f56
Arcos12 <roldan1212gomez@gmail.com>
2026-02-12 19:29:59 -0600

    Agregar archivo index.php y actualizar README.md con información institucional

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # test1_roldan
 repositorio para mostrar  mis squits trabajando con Git 
+
+## Institucion_educativa
+universidad Tecnologica de la selva 
+
+## lugar de procedencia
+Ocosingo Chiapas 2026
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Roldan Arcos G</title>
+</head>
+<body>
+    <h1>Bienvenido a mi página personal. Aquí encontrarás 
+    información sobre mis proyectos, habilidades y experiencia profesional.</h1>
+    
+</body>
+</html>
